6f1ba1cea86249b1b8c6faf41b12454962f5089d
GulajavaMinistudio <gulajava.mini@gmail.com>
2020-02-09 17:02:58 +0700

    WIP: buat kelas khusus timer

---
+++ src/TimerWaktu.js
@@ -0,0 +1,73 @@
+export default class TimerWaktu {
+    constructor(numberDurationInput, startButtonEl, pauseButtonEl, callbacks) {
+        this.numberDurationInput = numberDurationInput;
+        this.startButtonEl = startButtonEl;
+        this.pauseButtonEl = pauseButtonEl;
+
+        if (callbacks) {
+            this.onStartCallback = callbacks.onStart;
+            this.onTickCallback = callbacks.onTick;
+            this.onCompleteCallback = callbacks.onComplete;
+        }
+
+        this.intervalId = 0;
+
+        this.initListenerEl();
+    }
+
+    initListenerEl() {
+        // Menggunakan arrow function untuk memperbaiki masalah this scope
+        this.startButtonEl.addEventListener('click', event => {
+            this.startTimer(event);
+        });
+
+        this.pauseButtonEl.addEventListener('click', event => {
+            this.pauseTimer(event);
+        });
+    }
+
+    // this berisi nilai parameter dan context dimana this ini berada
+    // bisa berisi nilai dari kelas ataupun dari object atau bisa global window
+    startTimer() {
+        if (this.onStartCallback) {
+            this.onStartCallback();
+        }
+
+        this.tickWaktu();
+        this.intervalId = setInterval(() => {
+            this.tickWaktu();
+        }, 1000);
+    }
+
+    pauseTimer() {
+        // hentikan timer sementara waktu
+        clearInterval(this.intervalId);
+    }
+
+    onDurationChange() {}
+
+    tickWaktu() {
+        // Dengan cek getter dan setter
+        if (this.timeRemaining <= 0) {
+            this.pauseTimer();
+            if (this.onCompleteCallback) {
+                this.onCompleteCallback();
+            }
+        } else {
+            this.timeRemaining -= 1;
+            if (this.onTickCallback) {
+                this.onTickCallback();
+            }
+        }
+    }
+
+    // Dengan getter dan setter
+    // Menyimpan nilai di dalam DOM
+    get timeRemaining() {
+        return parseFloat(this.numberDurationInput.value);
+    }
+
+    set timeRemaining(time) {
+        this.numberDurationInput.value = time;
+    }
+}
--- src/apptimer.js
+++ src/apptimer.js
@@ -1,108 +1,207 @@
+/* eslint-disable max-classes-per-file */
 // TIMERAPP Membuat aplikasi Timer dengan JavaScript
-class Timer {
-    constructor(numberDurationInput, startButtonEl, pauseButtonEl) {
-        this.numberDurationInput = numberDurationInput;
+// class Timer {
+//     constructor(numberDurationInput, startButtonEl, pauseButtonEl) {
+//         this.numberDurationInput = numberDurationInput;
 
-        // karena ada keyword getter get, tidak perlu pake kurung buka tutup
-        this.numberSisaWaktu = this.getTimeRemaining();
+//         // karena ada keyword getter get, tidak perlu pake kurung buka tutup
+//         this.numberSisaWaktu = this.getTimeRemaining();
 
-        this.startButtonEl = startButtonEl;
-        this.pauseButtonEl = pauseButtonEl;
+//         this.startButtonEl = startButtonEl;
+//         this.pauseButtonEl = pauseButtonEl;
 
-        this.intervalId = 0;
+//         this.intervalId = 0;
 
-        this.initListenerEl();
-    }
+//         this.initListenerEl();
+//     }
 
-    initListenerEl() {
-        // Bermasalah dengan scope this karena this berisi nilai startButtonElement
-        // this.startButtonEl.addEventListener('click', function(event) {
-        //     console.log(this);
-        //     this.startTimer(event);
-        // });
+//     initListenerEl() {
+//         // Bermasalah dengan scope this karena this berisi nilai startButtonElement
+//         // this.startButtonEl.addEventListener('click', function(event) {
+//         //     console.log(this);
+//         //     this.startTimer(event);
+//         // });
 
-        // Dengan menggunakan bind untuk memperbaiki masalah this scope
-        // this.startButtonEl.addEventListener(
-        //     'click',
-        //     function(event) {
-        //         console.log(this);
-        //         console.log(event);
-        //     }.bind(this),
-        // );
+//         // Dengan menggunakan bind untuk memperbaiki masalah this scope
+//         // this.startButtonEl.addEventListener(
+//         //     'click',
+//         //     function(event) {
+//         //         console.log(this);
+//         //         console.log(event);
+//         //     }.bind(this),
+//         // );
 
-        // Menggunakan arrow function untuk memperbaiki masalah this scope
-        this.startButtonEl.addEventListener('click', event => {
-            this.startTimer(event);
-        });
+//         // Menggunakan arrow function untuk memperbaiki masalah this scope
+//         this.startButtonEl.addEventListener('click', event => {
+//             this.startTimer(event);
+//         });
 
-        this.pauseButtonEl.addEventListener('click', event => {
-            this.pauseTimer(event);
-        });
-    }
+//         this.pauseButtonEl.addEventListener('click', event => {
+//             this.pauseTimer(event);
+//         });
+//     }
 
-    // this berisi nilai parameter dan context dimana this ini berada
-    // bisa berisi nilai dari kelas ataupun dari object atau bisa global window
-    startTimer() {
-        this.onTick();
+//     // this berisi nilai parameter dan context dimana this ini berada
+//     // bisa berisi nilai dari kelas ataupun dari object atau bisa global window
+//     startTimer() {
+//         this.tickWaktu();
 
-        this.intervalId = setInterval(() => {
-            this.onTick();
-        }, 1000);
-    }
+//         this.intervalId = setInterval(() => {
+//             this.tickWaktu();
+//         }, 1000);
+//     }
 
-    stopTimer() {
-        console.log('Stop timer yang dimulai');
-        clearInterval(this.intervalId);
-    }
+//     stopTimer() {
+//         clearInterval(this.intervalId);
+//     }
 
-    pauseTimer() {
-        // hentikan timer sementara waktu
-        clearInterval(this.intervalId);
-    }
+//     pauseTimer() {
+//         // hentikan timer sementara waktu
+//         clearInterval(this.intervalId);
+//     }
 
-    onDurationChange() {}
+//     onDurationChange() {}
 
-    onTick() {
-        console.log('Waktu berdetik');
-        if (this.numberSisaWaktu > 0) {
-            // this.numberSisaWaktu -= 1;
-            // this.setTimeInput(this.numberSisaWaktu);
+//     tickWaktu() {
+//         // Dengan cek getter dan setter
+//         if (this.timeRemaining <= 0) {
+//             this.pauseTimer();
+//         } else {
+//             this.timeRemaining -= 1;
+//         }
+//     }
 
-            // dengan getter setter
-            this.timeRemaining -= 1;
-            this.numberSisaWaktu = this.timeRemaining;
-            // this.numberDurationInput.value = this.numberSisaWaktu - 1;
-        } else {
-            // this.numberSisaWaktu = 0;
-            // this.setTimeInput(this.numberSisaWaktu);
+//     onTick() {
+//         console.log('Waktu berdetik');
+//         if (this.numberSisaWaktu > 0) {
+//             // this.numberSisaWaktu -= 1;
+//             // this.setTimeInput(this.numberSisaWaktu);
 
-            // Dengan getter setter
-            this.timeRemaining = 0;
-            this.stopTimer();
-        }
-    }
+//             // dengan getter setter
+//             this.timeRemaining -= 1;
+//             this.numberSisaWaktu = this.timeRemaining;
+//             // this.numberDurationInput.value = this.numberSisaWaktu - 1;
+//         } else {
+//             // this.numberSisaWaktu = 0;
+//             // this.setTimeInput(this.numberSisaWaktu);
 
-    getTimeRemaining() {
-        return parseFloat(this.numberDurationInput.value);
-    }
+//             // Dengan getter setter
+//             this.timeRemaining = 0;
+//             this.stopTimer();
+//         }
+//     }
 
-    setTimeInput(numberTime) {
-        this.numberDurationInput.value = numberTime;
-    }
+//     getTimeRemaining() {
+//         return parseFloat(this.numberDurationInput.value);
+//     }
 
-    // Dengan getter dan setter
-    // Menyimpan nilai di dalam DOM
-    get timeRemaining() {
-        return parseFloat(this.numberDurationInput.value);
-    }
+//     setTimeInput(numberTime) {
+//         this.numberDurationInput.value = numberTime;
+//     }
 
-    set timeRemaining(time) {
-        this.numberDurationInput.value = time;
-    }
-}
+//     // Dengan getter dan setter
+//     // Menyimpan nilai di dalam DOM
+//     get timeRemaining() {
+//         return parseFloat(this.numberDurationInput.value);
+//     }
+
+//     set timeRemaining(time) {
+//         this.numberDurationInput.value = time;
+//     }
+// }
+
+// class TimerWaktu {
+//     constructor(numberDurationInput, startButtonEl, pauseButtonEl, callbacks) {
+//         this.numberDurationInput = numberDurationInput;
+//         this.startButtonEl = startButtonEl;
+//         this.pauseButtonEl = pauseButtonEl;
+
+//         if (callbacks) {
+//             this.onStartCallback = callbacks.onStart;
+//             this.onTickCallback = callbacks.onTick;
+//             this.onCompleteCallback = callbacks.onComplete;
+//         }
+
+//         this.intervalId = 0;
+
+//         this.initListenerEl();
+//     }
+
+//     initListenerEl() {
+//         // Menggunakan arrow function untuk memperbaiki masalah this scope
+//         this.startButtonEl.addEventListener('click', event => {
+//             this.startTimer(event);
+//         });
+
+//         this.pauseButtonEl.addEventListener('click', event => {
+//             this.pauseTimer(event);
+//         });
+//     }
+
+//     // this berisi nilai parameter dan context dimana this ini berada
+//     // bisa berisi nilai dari kelas ataupun dari object atau bisa global window
+//     startTimer() {
+//         if (this.onStartCallback) {
+//             this.onStartCallback();
+//         }
+
+//         this.tickWaktu();
+//         this.intervalId = setInterval(() => {
+//             this.tickWaktu();
+//         }, 1000);
+//     }
+
+//     pauseTimer() {
+//         // hentikan timer sementara waktu
+//         clearInterval(this.intervalId);
+//     }
+
+//     onDurationChange() {}
+
+//     tickWaktu() {
+//         // Dengan cek getter dan setter
+//         if (this.timeRemaining <= 0) {
+//             this.pauseTimer();
+//             if (this.onCompleteCallback) {
+//                 this.onCompleteCallback();
+//             }
+//         } else {
+//             this.timeRemaining -= 1;
+//             if (this.onTickCallback) {
+//                 this.onTickCallback();
+//             }
+//         }
+//     }
+
+//     // Dengan getter dan setter
+//     // Menyimpan nilai di dalam DOM
+//     get timeRemaining() {
+//         return parseFloat(this.numberDurationInput.value);
+//     }
+
+//     set timeRemaining(time) {
+//         this.numberDurationInput.value = time;
+//     }
+// }
+
+// Dengan ES2015 Import
+import TimerWaktu from './TimerWaktu';
 
 const durasiInputEl = document.querySelector('#input_duration');
 const startButtonEl = document.querySelector('#button_start');
 const pauseButtonEl = document.querySelector('#button_pause');
 
-const timerWaktu = new Timer(durasiInputEl, startButtonEl, pauseButtonEl);
+// const timerWaktu = new TimerWaktu(durasiInputEl, startButtonEl, pauseButtonEl);
+
+// Menggunakan Callback
+const timerWaktu = new TimerWaktu(durasiInputEl, startButtonEl, pauseButtonEl, {
+    onStart() {
+        console.log('Timer dimulai callback');
+    },
+    onTick() {
+        console.log('Timer sedang berdetik');
+    },
+    onComplete() {
+        console.log('Timer sudah selesai');
+    },
+});
